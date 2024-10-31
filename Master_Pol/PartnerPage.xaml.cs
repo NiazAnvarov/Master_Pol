3590d6e2a6e9e7747ee90c8dd2a0333f6c0f4644
niaza <niaza@LAPTOP-90N45E5Q>
2024-10-31 22:34:58 +0500

    Добавьте файлы проекта.

---
+++ Master_Pol/PartnerPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Master_Pol
+{
+    /// <summary>
+    /// Логика взаимодействия для PartnerPage.xaml
+    /// </summary>
+    public partial class PartnerPage : Page
+    {
+        public PartnerPage()
+        {
+            InitializeComponent();
+
+            List<Partner> currentPartners = Anvarov_master_polEntities.GetContext().Partner.ToList();
+
+            PartnerListView.ItemsSource = currentPartners;
+
+        }
+    }
+}
